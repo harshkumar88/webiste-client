6eb932d12ab37faeaaa36a1192d4cc058d7bdde7
Harsh <harsh@Harshs-MacBook-Air.local>
2024-11-25 22:41:55 +0530

    p

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  publicDir: "public", // Ensure this is correct if you're using a custom folder
+});
